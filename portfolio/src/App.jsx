5122deab11001c726ead037365d58b279cf027d7
franck-tchassi <franck.tchassi@laplateforme.io>
2024-07-31 19:35:24 +0200

    importation des composants au fichier App.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import Contact from './components/contact/Contact';
+import { ToastContainer } from 'react-toastify';
+import Accueil from './components/accueil/Accueil';
+import Footer from './components/footer/Footer';
+import Presentation from './components/presentation/Presentation';
+import Services from './components/services/Services';
+import Realisation from './components/realisation/Realisation';
+import Navbar from './components/navbar/Navbar';
+import Card from './components/card/Card';
+import './app.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedTravail, setSelectedTravail] = useState(null);
+
+  const handleOpenModal = (travail) => {
+    setSelectedTravail(travail);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setSelectedTravail(null);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ToastContainer />
+      <Navbar />
+      <Accueil />
+      <Presentation />
+      <Services />
+      <Realisation handleOpenModal={handleOpenModal} />
+      <Contact />
+      <Footer />
+      
+      {isModalOpen && (
+        <div className='modal'>
+          <div className='modal_overlay' onClick={handleCloseModal}></div>
+          <div className='modal_content'>
+            <Card travail={selectedTravail} closeModal={handleCloseModal} />
+          </div>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
